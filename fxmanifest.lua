ef9c48218e8b319eaa2978b53676df7d98724f29
HR0110 <hrisiraya@abv.bg>
2024-12-10 19:03:29 +0200

    feat(fxmanifest.lua): repository description

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,7 @@ lua54 'yes'
 name 'HRWhitelist'
 author 'HRScripts Development'
 description 'A discord whitelist resource'
+repository 'https://github.com/HRScripts/HRWhitelist'
 version '1.0.1'
 
 shared_script '@HRLib/import.lua'
